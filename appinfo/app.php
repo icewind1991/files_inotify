4830e8a264f39d16865c07704d173ea4687530b2
Robin Appelman <robin@icewind.nl>
2017-02-13 15:06:28 +0100

    Fix nc12 compatibility

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,6 +3,6 @@
 
 
 // Mock the Nextcloud 12 notify classes and interfaces when running on Nextcloud 11
-if (!class_exists('OCP\Files\Notify\INotifyHandler')) {
+if (!interface_exists('\OCP\Files\Notify\INotifyHandler')) {
 	require_once 'compat.php';
 }
