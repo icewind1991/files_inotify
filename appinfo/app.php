9bac17bfce13e155127636060f50bced490c3631
Robin Appelman <robin@icewind.nl>
2018-05-14 14:17:16 +0200

    remove nc11 compat
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,8 +1,2 @@
 <?php
 (new \OCA\Files_INotify\AppInfo\Application())->register();
-
-
-// Mock the Nextcloud 12 notify classes and interfaces when running on Nextcloud 11
-if (!interface_exists('\OCP\Files\Notify\INotifyHandler')) {
-	require_once 'compat.php';
-}
