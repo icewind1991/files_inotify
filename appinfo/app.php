3052ae8e1317d625307e5a164229f595520f7b75
Robin Appelman <robin@icewind.nl>
2018-08-24 17:19:57 +0200

    dont break nextcloud when files_exernal is not enabled
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,2 +1,4 @@
 <?php
-(new \OCA\Files_INotify\AppInfo\Application())->register();
+if (interface_exists(\OCA\Files_External\Lib\Config\IBackendProvider::class)) {
+	(new \OCA\Files_INotify\AppInfo\Application())->register();
+}
